64a6c6ec4836a2166e004f5a64f95a8de8da7498
georgewhite997 <george.white997@tutamail.com>
2025-05-02 18:13:48 +0200

    fix

--- contracts/Skins.sol
+++ contracts/Skins.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "./Ownable.sol";
 import "./IERC20.sol";
 
@@ -8,13 +7,18 @@ contract Skins is Ownable {
     address public paymentToken;
 
     mapping(uint16 => uint256) public skinPrice;
-    mapping(address => uint16[]) public userSkins;
     mapping(address => mapping(uint16 => bool)) private owns;
 
-    event SkinPriceSet(uint16 indexed skinId, uint256 price);
-    event SkinPurchased(address indexed buyer, uint16 indexed skinId, uint256 price);
+    event SkinPurchased(
+        address indexed buyer,
+        uint16 indexed skinId,
+        uint256 price
+    );
 
-    constructor(address _initialOwner, address _paymentToken) Ownable(_initialOwner) {
+    constructor(
+        address _initialOwner,
+        address _paymentToken
+    ) Ownable(_initialOwner) {
         paymentToken = _paymentToken;
     }
 
@@ -24,22 +28,29 @@ contract Skins is Ownable {
 
     function setSkinPrice(uint16 skinId, uint256 price) external onlyOwner {
         skinPrice[skinId] = price;
-        emit SkinPriceSet(skinId, price);
     }
 
     function purchase(uint16 skinId) external {
         uint256 price = skinPrice[skinId];
         require(price != 0, "Skin not for sale");
         require(!owns[msg.sender][skinId], "Already owned");
-        require(IERC20(paymentToken).transferFrom(msg.sender, address(this), price), "Token transfer failed");
-
+        require(
+            IERC20(paymentToken).transferFrom(msg.sender, address(this), price),
+            "Token transfer failed"
+        );
         owns[msg.sender][skinId] = true;
-        userSkins[msg.sender].push(skinId);
         emit SkinPurchased(msg.sender, skinId, price);
     }
 
-    function hasSkin(address user, uint16 skinId) external view returns (bool) {
-        return owns[user][skinId];
+    function getOwnedSkins(
+        address user,
+        uint16[] calldata skinIds
+    ) external view returns (bool[] memory) {
+        bool[] memory result = new bool[](skinIds.length);
+        for (uint256 i = 0; i < skinIds.length; i++) {
+            result[i] = owns[user][skinIds[i]];
+        }
+        return result;
     }
 
     function withdraw() external onlyOwner {
