d1ed491b841c8773a648c76e8c976f66c9e9f087
Freshy <206606286+michalwhite123@users.noreply.github.com>
2025-05-12 13:06:34 +0200

    leadboard loading

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -42,7 +42,9 @@ export const Leaderboard = ({
 
         }
 
-        fetchLeaderboard();
+        setTimeout(() => {
+            fetchLeaderboard();
+        }, 300);
     }, [])
 
     return (
@@ -99,7 +101,7 @@ export const Leaderboard = ({
                             </div>
                         </>
                     ) : (
-                        <div>Loading data...</div>
+                        <SkeletonLeaderboard />
                     )}
                 </div>
             </div>
@@ -111,12 +113,77 @@ export const Position = (
     { leaderboardPosition }: { leaderboardPosition: LeaderboardPosition }
 ) => {
     return (
-        <div className={`${leaderboardPosition.position > 4 ? 'mt-[4px]' : ''} flex justify-between`}>
+        <div className={`${leaderboardPosition.position > 4 ? 'mt-[4px]' : ''} flex justify-between items-center`}>
             <div className="flex items-center justify-center ">
                 <div className={`flex items-center justify-center ${leaderboardPosition.position > 10 ? 'bg-blue-500' : 'bg-green-500'} w-[33px] h-[33px] rounded-md border-[1px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] text-[14px]`}>{leaderboardPosition.position}</div>
                 <div className="ml-[8px]">{leaderboardPosition.wallet === 'none' ? 'none' : displayAddress(leaderboardPosition.wallet)}</div>
             </div>
             <div>{leaderboardPosition.wallet === 'none' ? 'none' : formatScore(leaderboardPosition.highestScore) + 'm'}</div>
         </div>
+    )
+}
+
+const SkeletonBox = ({ width = '100%', height = '20px', className = '' }) => (
+    <div
+        className={`animate-pulse [animation-duration:1.2s] bg-gray-200 rounded-md ${className}`}
+        style={{ width, height }}
+    />
+);
+
+export const PositionSkeleton = (
+    { position }: { position: number }
+) => {
+    return (
+        <div className={`${position > 4 ? 'mt-[4px]' : ''} flex justify-between items-center`}>
+            <div className="flex items-center justify-center ">
+                <div className={`flex items-center justify-center ${position > 10 ? 'bg-blue-500' : 'bg-green-500'} w-[33px] h-[33px] rounded-md border-[1px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] text-[14px]`}>{position}</div>
+                <SkeletonBox className="ml-[8px]" width="60px" />
+            </div>
+            <SkeletonBox className="ml-[8px]" width="100px" />
+        </div>
+    )
+}
+
+const SkeletonLeaderboard = () => {
+    return (
+        <>
+            <div className="flex justify-between">
+                <div className="flex h-[153px] flex-col items-center justify-center px-[10px] pt-[6px] pb-[8px] rounded-md border-[2px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] bg-gradient-to-b from-[#FEED88] to-[#FFAB2B]">
+                    {/* <div className="text-[16px]">{leaderboard[0] ? displayAddress(leaderboard[0].wallet) : 'none'}</div> */}
+                    <SkeletonBox />
+                    <img className="mt-[8px]" width={74} height={74} src="/1stplace.png" alt="1stplaceimg" />
+                    <SkeletonBox className="mt-[8px]" />
+                </div>
+
+                <div className="flex h-[153px] flex-col items-center justify-center px-[10px] pt-[6px] pb-[8px] rounded-md border-[2px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] bg-gradient-to-b from-[#FDF4FB] to-[#A8B5BD]">
+                    <SkeletonBox />
+                    <img className="mt-[8px]" width={74} height={74} src="/2ndplace.png" alt="1stplaceimg" />
+                    <SkeletonBox className="mt-[8px]" />
+                </div>
+
+                <div className="flex h-[153px] flex-col items-center justify-center px-[10px] pt-[6px] pb-[8px] rounded-md border-[2px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] bg-gradient-to-b from-[#FFC790] to-[#CB6326]">
+                    <SkeletonBox />
+                    <img className="mt-[8px]" width={74} height={74} src="/3rdplace.png" alt="1stplaceimg" />
+                    <SkeletonBox className="mt-[8px]" />
+                </div>
+            </div>
+
+            <div className="mt-10 relative w-full">
+                <img src="/button-cover.svg" className="absolute z-[9] top-[-24px] left-1/2 -translate-x-1/2  min-w-[108%]" alt="snow" />
+                <div
+                    className="w-full text-[20px] p-[16px] rounded-sm border-[2px] border-[#030303] shadow-[0px_2px_0px_rgba(0,0,0,0.45)] bg-[#7FCBDC]"
+                >
+                    {[
+                        ...Array.from({ length: 12 }, (_, i) => ({
+                            wallet: 'none',
+                            position: i + 4,
+                            highestScore: 0,
+                        }))
+                    ].map((leaderboardPosition, index) => (
+                        <PositionSkeleton key={index} position={leaderboardPosition.position} />
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
